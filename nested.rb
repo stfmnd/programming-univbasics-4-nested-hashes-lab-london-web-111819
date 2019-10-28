8ca325d640a7fdf2e1d40568791ca5c87d344ac3
Matthew <matthew.97.clark@gmail.com>
2019-10-28 00:26:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     return programmer_hash[:dennis_ritchie][:languages]
+     return programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz

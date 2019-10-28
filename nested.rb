7ed360e259f5575e22994d218428e0b6c36c8495
Matthew <matthew.97.clark@gmail.com>
2019-10-28 00:32:15 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,12 +16,12 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper];
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,6 +84,9 @@ def adding_matz
         }
      }
 
+    programmer_hash[:yukihiro_matsumoto] = {}
+    
+    return programmer_hash
     
 end
 
@@ -102,6 +109,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     return programmer_hash
+     
      
 end
 
@@ -121,5 +132,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push('Assembly')
+     
+     return programmer_hash
 
 end
